c7b6e3be8189f459cfae9769a743c382b369f01d
cam <camnewnham@gmail.com>
2023-10-06 17:30:10 +1100

    finish basic visuals and slider functionality

--- Plugin/TimelineComponent.cs
+++ Plugin/TimelineComponent.cs
@@ -1,9 +1,7 @@
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Special;
-using Grasshopper.Kernel.Types;
 using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Plugin
@@ -12,26 +10,18 @@ namespace Plugin
     {
         public override Guid ComponentGuid => new Guid("84e977ef-b06d-41e3-aa6d-c6f0f646cef3");
         protected override System.Drawing.Bitmap Icon => null;
-
-        public readonly Timeline Timeline;
         public override GH_ParamKind Kind => GH_ParamKind.floating;
 
         public TimelineComponent() : base()
         {
-            Timeline = new Timeline();
             Slider.DecimalPlaces = 8;
             Slider.Minimum = 0;
             Slider.Maximum = 1;
-            Slider.ValueChanged += OnSliderValueChanged;
-        }
+            NickName = "Time";
 
-        private void OnSliderValueChanged(object sender, Grasshopper.GUI.Base.GH_SliderEventArgs e)
-        {
-            Timeline.Time = (double)e.Slider.Value;
         }
 
         public override string Name => "Timeline";
-        public override string NickName => "Timeline";
         public override string Description => "Displays keyframes for animating your definition.";
 
         public override string Category => "Display";
@@ -54,11 +44,9 @@ namespace Plugin
                         Debug.Assert(doc != null, "Can not start recording; document was not found.");
                         Debug.Assert(stopRecordingAction == null, "Can not start recording; stop recording action must be null.");
                         doc.SolutionEnd += OnSolutionEndRecordState;
-                        doc.SolutionStart += OnSolutionStartRecordState;
                         stopRecordingAction = () =>
                         {
                             doc.SolutionEnd -= OnSolutionEndRecordState;
-                            doc.SolutionStart -= OnSolutionStartRecordState;
                         };
                     }
                     else
@@ -74,38 +62,6 @@ namespace Plugin
         public override void CreateAttributes()
         {
             Attributes = new TimelineComponentAttributes(this);
-        }
-
-        protected override void OnVolatileDataCollected()
-        {
-            double time = 0;
-            if (VolatileDataCount > 0)
-            {
-                IGH_Goo goo = VolatileData.AllData(true).First();
-                if (!goo.IsValid)
-                {
-                    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, goo.IsValidWhyNot);
-                    return;
-                };
-                if (goo.CastTo(out double result))
-                {
-                    if (result < 0 || result > 1)
-                    {
-                        AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Time must be between 0 and 1.");
-                        result = Math.Min(1, Math.Max(0, result));
-                    }
-                    time = result;
-                }
-                else
-                {
-                    AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Unable to convert to number.");
-                }
-            }
-            if (VolatileDataCount > 1)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Only one input value between 0 and 1 is supported.");
-            }
-            Timeline.Time = time;
         }
 
         public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
@@ -142,33 +98,9 @@ namespace Plugin
             base.RemovedFromDocument(document);
         }
 
-        private bool recordNextState = false;
         private void OnSolutionEndRecordState(object sender, GH_SolutionEventArgs e)
         {
-            // Ignore components that are upstream of this component
-            if (recordNextState)
-            {
-                Rhino.RhinoApp.WriteLine("Record state!");
-            }
-            else
-            {
-                Rhino.RhinoApp.WriteLine("Do not record state!");
-            }
-        }
-
-        private void OnSolutionStartRecordState(object sender, GH_SolutionEventArgs e)
-        {
-            // Skip updates where only this component and/or the input slider have changed
-            foreach (IGH_DocumentObject obj in e.Document.Objects)
-            {
-                if (obj != this && obj is IGH_ActiveObject activeObj && activeObj.Phase == GH_SolutionPhase.Blank && !DependsOn(activeObj))
-                {
-                    recordNextState = true;
-                    return;
-                }
-            }
-            recordNextState = false;
-            return;
+            // TODO create an entry.
         }
     }
 }
--- Plugin/TimelineComponentAttributes.cs
+++ Plugin/TimelineComponentAttributes.cs
@@ -1,4 +1,5 @@
-﻿using Grasshopper.GUI;
+﻿using Grasshopper;
+using Grasshopper.GUI;
 using Grasshopper.GUI.Canvas;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Attributes;
@@ -20,28 +21,23 @@ namespace Plugin
             Bounds = (RectangleF)new Rectangle(0, 0, 250, 64);
         }
 
-        public RectangleF TimelineBounds
-        {
-            get
-            {
-                RectangleF box = Bounds;
-                box.Inflate(-6f, -6f);
-                return box;
-            }
-        }
-        public RectangleF RecordingBounds
-        {
-            get
-            {
-                RectangleF box = Bounds;
-                box.Inflate(6f, 6f);
-                return box;
-            }
-        }
+        public RectangleF ProgressTextBounds { get; private set; }
+        public RectangleF ContentBounds { get; private set; }
+        public RectangleF ProgressGrabBarBounds { get; private set; }
+        public float CurrentTimeXPosition { get; private set; }
 
         protected override void Layout()
         {
-            Bounds = (RectangleF)GH_Convert.ToRectangle(new RectangleF(Pivot, Bounds.Size));
+            Bounds = new RectangleF(Pivot, Bounds.Size);
+
+            RectangleF timelineBounds = Bounds;
+            timelineBounds.Inflate(-5f, -5f);
+            ContentBounds = timelineBounds;
+
+            CurrentTimeXPosition = (float)Remap((double)Owner.CurrentValue, 0, 1, ContentBounds.X, ContentBounds.X + ContentBounds.Width);
+            ProgressGrabBarBounds = new RectangleF(CurrentTimeXPosition - 2, ContentBounds.Y, 4, ContentBounds.Height);
+
+            ProgressTextBounds = new RectangleF(CurrentTimeXPosition - 24 / 2f, ContentBounds.Top - 22f, 24, 12);
         }
 
         protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
@@ -59,7 +55,7 @@ namespace Plugin
                         break;
                     }
 
-                    GH_Palette palette = GH_Palette.Hidden;
+                    GH_Palette palette = GH_Palette.Normal;
                     switch (Owner.RuntimeMessageLevel)
                     {
                         case GH_RuntimeMessageLevel.Warning:
@@ -76,29 +72,204 @@ namespace Plugin
 
                     using (GH_Capsule capsule = GH_Capsule.CreateCapsule(Bounds, palette))
                     {
-                        //capsule.SetJaggedEdges(true, true);
-                        //capsule.AddInputGrip(InputGrip.Y);
                         capsule.AddOutputGrip(OutputGrip.Y);
-
                         graphics.SmoothingMode = SmoothingMode.HighQuality;
                         capsule.Render(graphics, Selected, Owner.Locked, false);
                     }
 
-                    Rectangle rectangle = GH_Convert.ToRectangle(TimelineBounds);
-                    //Owner.Gradient.Render_Background(graphics, (RectangleF)rectangle);
-                    //Owner.Gradient.Render_Gradient(graphics, (RectangleF)rectangle);
+                    Rectangle rectangle = GH_Convert.ToRectangle(ContentBounds);
+
                     if (Owner.Recording)
                     {
-                        graphics.FillRectangle(new SolidBrush(Color.FromArgb(200, 0, 0)), rectangle);
+                        graphics.FillRectangle(new SolidBrush(Color.FromArgb(200, 0, 0)), ContentBounds);
                     }
-                    Owner.Timeline.RenderCurrentTime(graphics, rectangle);
+
+                    RenderBackground(graphics);
                     GH_GraphicsUtil.ShadowRectangle(graphics, rectangle);
+                    RenderCurrentTime(graphics);
                     graphics.DrawRectangle(Pens.Black, rectangle);
-                    //Owner.Gradient.Render_Grips(graphics, (RectangleF)rectangle);
-                    //GH_PaletteStyle impliedStyle = GH_CapsuleRenderEngine.GetImpliedStyle(palette, Selected, Owner.Locked, false);
-                    //GH_ComponentAttributes.RenderComponentParameters(canvas, graphics, Owner, impliedStyle);
                     break;
             }
         }
+
+        private static double Remap(double value, double low1, double high1, double low2, double high2)
+        {
+            return low2 + (value - low1) * (high2 - low2) / (high1 - low1);
+        }
+
+        public void RenderCurrentTime(Graphics graphics)
+        {
+            int lineAlpha = GH_Canvas.ZoomFadeLow;
+            if (lineAlpha < 5)
+            {
+                return;
+            }
+
+            // Draw Vertical line
+            using (Pen pen = new Pen(Color.FromArgb(GH_Canvas.ZoomFadeLow, Color.White), 2f))
+            {
+                GH_GraphicsUtil.ShadowVertical(graphics, CurrentTimeXPosition, ContentBounds.Bottom, ContentBounds.Top, 8f, true, 15);
+                GH_GraphicsUtil.ShadowVertical(graphics, CurrentTimeXPosition, ContentBounds.Bottom, ContentBounds.Top, 8f, false, 15);
+                graphics.DrawLine(pen, CurrentTimeXPosition, ContentBounds.Bottom, CurrentTimeXPosition, ContentBounds.Top);
+            }
+
+            if (m_isDraggingSlider || GH_Canvas.ZoomFadeHigh > 0)
+            {
+                // Draw text capsule tooltip
+
+                string content = (Owner.CurrentValue * 100).ToString("00.0");
+                if (Owner.CurrentValue == 1)
+                {
+                    content = "100";
+                }
+
+                int textAlpha = m_isDraggingSlider ? 255 : GH_Canvas.ZoomFadeHigh;
+
+                using (SolidBrush fill = new SolidBrush(Color.FromArgb((int)(textAlpha * (200f / 255f)), Color.Black)))
+                {
+                    using (GraphicsPath path = GH_CapsuleRenderEngine.CreateRoundedRectangle(ProgressTextBounds, 2f))
+                    {
+                        path.AddPolygon(new PointF[]
+                        {
+                        new PointF(CurrentTimeXPosition,ProgressTextBounds.Bottom + 3f),
+                        new PointF(CurrentTimeXPosition-3f,ProgressTextBounds.Bottom),
+                        new PointF(CurrentTimeXPosition+3f,ProgressTextBounds.Bottom)
+                        });
+
+                        path.FillMode = FillMode.Winding;
+                        graphics.FillPath(fill, path);
+                    }
+                }
+
+                using (SolidBrush brush = new SolidBrush(Color.FromArgb(textAlpha, Color.White)))
+                {
+                    graphics.TextRenderingHint = GH_TextRenderingConstants.GH_SmoothText;
+                    graphics.DrawString(content, GH_FontServer.Small, brush, ProgressTextBounds, GH_TextRenderingConstants.CenterCenter);
+                }
+            }
+        }
+
+        private void RenderBackground(Graphics graphics)
+        {
+            if (GH_Canvas.ZoomFadeLow < 1)
+            {
+                return;
+            }
+
+            const int segmentCount = 10;
+            using (Pen pen = new Pen(Color.FromArgb(20 * (int)(GH_Canvas.ZoomFadeLow / 255f), Color.Black), 1f))
+            {
+                for (int i = 1; i < segmentCount; i++)
+                {
+                    float x = (float)Remap(i, 0, segmentCount, ContentBounds.Left, ContentBounds.Right);
+                    graphics.DrawLine(pen, x, ContentBounds.Bottom, x, ContentBounds.Top);
+                }
+            }
+            if (GH_Canvas.ZoomFadeMedium < 1)
+            {
+                return;
+            }
+            using (Pen pen = new Pen(Color.FromArgb(10 * (int)(GH_Canvas.ZoomFadeMedium / 255f), Color.Black), 1f))
+            {
+                for (int i = 1; i < segmentCount * 5; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        continue;
+                    }
+
+                    float x = (float)Remap(i, 0, segmentCount * 5, ContentBounds.Left, ContentBounds.Right);
+                    graphics.DrawLine(pen, x, ContentBounds.Bottom, x, ContentBounds.Top);
+                }
+            }
+        }
+
+
+        public static void RenderGrip(Graphics graphics, float x, float y, float radius = 5, float stroke = 1)
+        {
+            int alpha = GH_Canvas.ZoomFadeLow;
+            if (alpha < 5)
+            {
+                return;
+            }
+
+            using (SolidBrush white = new SolidBrush(Color.FromArgb(alpha, Color.White)))
+            {
+                using (SolidBrush black = new SolidBrush(Color.FromArgb(alpha, Color.Black)))
+                {
+                    graphics.FillEllipse(black, x - radius, y - radius, 2 * radius, 2 * radius);
+                    graphics.FillEllipse(white, x - radius + stroke, y - radius + stroke, 2 * (radius - stroke), 2 * (radius - stroke));
+                }
+            }
+        }
+
+        #region Mouse
+
+        private PointF m_mousePosition;
+        private PointF m_mouseDelta;
+        private bool m_isDraggingSlider;
+
+        public bool IsMouseOverSliderHandle()
+        {
+            return ProgressGrabBarBounds.Contains(m_mousePosition);
+        }
+
+        public override GH_ObjectResponse RespondToMouseMove(GH_Canvas sender, GH_CanvasMouseEvent e)
+        {
+            m_mouseDelta = new PointF(e.CanvasX - m_mousePosition.X, e.CanvasY - m_mousePosition.Y);
+            m_mousePosition = e.CanvasLocation;
+
+            switch (e.Button)
+            {
+                case MouseButtons.None:
+                    if (IsMouseOverSliderHandle())
+                    {
+                        Instances.CursorServer.AttachCursor(sender, "GH_NumericSlider");
+                        return GH_ObjectResponse.Handled;
+                    }
+                    break;
+                case MouseButtons.Left:
+                    if (m_isDraggingSlider)
+                    {
+                        float pctChange = m_mouseDelta.X / ContentBounds.Width;
+                        Owner.SetSliderValue((decimal)((double)Owner.CurrentValue + pctChange));
+                        //ExpireLayout();
+                        //Instances.ActiveCanvas.Invalidate();
+                        Owner.ExpireSolution(true);
+                        return GH_ObjectResponse.Handled;
+                    }
+                    break;
+            }
+            return base.RespondToMouseMove(sender, e);
+        }
+
+        public override GH_ObjectResponse RespondToMouseUp(GH_Canvas sender, GH_CanvasMouseEvent e)
+        {
+            if (e.Button == MouseButtons.Left && m_isDraggingSlider)
+            {
+                m_isDraggingSlider = false;
+                Instances.ActiveCanvas.Invalidate();
+                return GH_ObjectResponse.Release;
+            }
+            return base.RespondToMouseUp(sender, e);
+        }
+
+        public override GH_ObjectResponse RespondToMouseDown(GH_Canvas sender, GH_CanvasMouseEvent e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    if (IsMouseOverSliderHandle())
+                    {
+                        m_isDraggingSlider = true;
+                        Grasshopper.Instances.ActiveCanvas.Invalidate();
+                        return GH_ObjectResponse.Capture;
+                    }
+                    break;
+            }
+
+            return base.RespondToMouseDown(sender, e);
+        }
+        #endregion
     }
 }
